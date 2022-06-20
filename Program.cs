2724a234aea9921bdf1c15695a94d3f97c190bb0
André Gois <andresousalee@gmail.com>
2022-06-20 10:05:32 -0300

    :tada: first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationVSCode
+{
+    class Program {
+        static void Main(String[] args){
+            Console.WriteLine("Hello world");
+            SByte a = 124;
+            System.Console.WriteLine("a: "+a);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+# Aplicação Console C#
+
+
+## Cria aplicação
+- dotnet new console --language C#
+## Roda aplicação
+- dotnet run
