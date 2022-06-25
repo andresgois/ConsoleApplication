d341f03cdb6ff21b1cf9a262c22b3b67db641b83
André Gois <andresousalee@gmail.com>
2022-06-25 16:23:37 -0300

    :recycle: refactor: data input

--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConsoleApplicationVSCode
 {
     class Program {
         static void Main(String[] args){
-            Console.WriteLine("Hello world");
+            /*Console.WriteLine("Hello world");
             SByte a = 124;
             System.Console.WriteLine("a: "+a);
+            Console.WriteLine("What is your name? ");
+            String name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            // NewLine -> representa uma quebra de linha.
+            System.Console.WriteLine($"{Environment.NewLine} Hello, {name}, on {currentDate.Date}");
+            System.Console.WriteLine($"{Environment.NewLine} Press any key to exit...");
+            // Espera que uma tecla seja clicada para sair
+
+            string nome = "Andre";
+            SByte idade = 33;
+            float salario = 2.1478F;
+            Console.WriteLine($"Meu nome é {nome} de idade {idade}, cujo, salário é {salario:F2.CultureInfo.InvariantCulture}");
+
+
+            Console.ReadKey(true);*/
+
+            // Aula 02
+            /*
+            string produto1 = "Compoutador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.5;
+            double preco3 = 53.234567;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2:F2}");
+
+            Console.WriteLine();
+            
+            Console.WriteLine($"Registro: ${idade} anos de idade, código {codigo} e gênero: {genero}");
+            
+            Console.WriteLine();
+            
+            Console.WriteLine("Medida com oito casas decimais: "+preco3.ToString("F8"));
+            Console.WriteLine("Arredondamento (Três casa decimais): "+preco3.ToString("F3"));
+            Console.WriteLine("separador deciaml invariant culture: "+preco3.ToString("F3", CultureInfo.InvariantCulture));
+            */
+
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 - dotnet new console --language C#
 ## Roda aplicação
 - dotnet run
+
+
+## Depurar um aplicativo de console do .NET usando Visual Studio Code
+- Run and debbug
+- Create launch
+- Abra .vscode/launch.json.
+- Altere a console configuração de internalConsole para integratedTerminal:
+```
+"console": "integratedTerminal",
+```
+- Salve suas alterações.
+- Coloque um break ponit no código
+- Clique no botão "Run and debbug" na lateral
+- Start Debuging
+## Referências
+[Documentação](https://docs.microsoft.com/pt-br/dotnet/)
+[Debugging](https://docs.microsoft.com/pt-br/dotnet/core/tutorials/debugging-with-visual-studio-code?pivots=dotnet-6-0)
